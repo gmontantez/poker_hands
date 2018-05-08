1b31f92e428cd279c34244203d8f8ab36a9216a8
gmontantez <gmontantez7@gmail.com>
2018-05-08 16:49:13 -0400

    Test Hand Comparison Three of a Kind Tie

--- poker_hands_test.rb
+++ poker_hands_test.rb
@@ -301,6 +301,13 @@ class Poker_hands_test < Minitest::Test
 	 	assert_equal("hand1 is the winner",hand_comparison(hands))
 	end
 
+	def test_hand_comparison_three_of_a_kind_tie
+		hand1 = ["4s", "4d", "4h", "6h", "5h"]
+		hand2 = ["3h", "7s", "5h", "7d", "7h"]
+		hands = {"hand1" => hand1, "hand2" => hand2}  
+	 	assert_equal("hand2 is the winner",hand_comparison(hands))
+	end
+
 	# def test_deal_with_tie_using_royal
 	# 	hand1 = ["Qh", "Qd", "4h", "5h", "6h"]
 	# 	hand2 = ["3h", "4h", "Th", "Td", "6h"] 
